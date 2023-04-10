e60dbc9ab61cb41306a11422bfb419625c5b7f14
annbinus <annbinus@knights.ucf.edu>
2023-04-10 15:01:09 -0400

    Deleted unnecessary stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
- 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
        
-      </header>
+
     </div>
   );
 }
