35cd375d93c553b88e6e7168ab430b8312dfb336
Gabriel De Oliveira <gabrieldeo03@gmail.com>
2023-04-10 14:52:55 -0400

    added basic CRUD routes

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -21,4 +21,34 @@ router.route('/add').post((req, res) =>
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) =>
+{
+    Task.findById(req.params.id)
+        .then(task => res.json(task))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) =>
+{
+    Task.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Task deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) =>
+{
+    Task.findById(req.params.id)
+        .then(task =>
+        {
+            task.name = req.body.name;
+            task.start = req.body.start;
+            task.completed = req.body.completed;
+
+            task.save()
+                .then(() => res.json('Task updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -20,4 +20,32 @@ router.route('/add').post((req, res) =>
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) =>
+{
+    User.findById(req.params.id)
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) =>
+{
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json('User deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) =>
+{
+    User.findById(req.params.id)
+        .then(user =>
+        {
+            user.username = req.body.username;
+
+            user.save()
+                .then(() => res.json('User updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
