282aeffee85db689e714c52afa786466f7fab9d7
Curbeks <andyjh1991@yahoo.com>
2023-04-22 02:24:08 -0400

    Changed sidebar to go to bottom and be fixed

--- src/Components/SidebarData.js
+++ src/Components/SidebarData.js
@@ -7,22 +7,18 @@ import LogoutIcon from '@mui/icons-material/Logout';
 // Data for sidebar in array form containing objects
 export const SidebarData = [
     {
-        name: "Dashboard",
         icon: <SpaceDashboardIcon />,
         link: "/Dashboard"
     },
     {
-        name: "Add Users",
         icon: <PersonAddIcon />,
         link: "/AddUsers"
     },
     {
-        name: "Settings",
         icon: <SettingsIcon />,
         link: "/Settings"
     },
     {
-        name: "Sign out",
         icon: <LogoutIcon />,
         link: "/SignIn"
     },
