140dec0ed0b1e146b8601a40f8b8f4d98b05f8c0
Gabriel De Oliveira <gabrieldeo03@gmail.com>
2023-04-22 21:50:10 -0400

    Revert "Merge branch 'master' of https://github.com/annbinus/LargeProjectGroup5COP4331"
    This reverts commit 2de5b7c88cdede4ed9f0393d1c3303a01d447173, reversing
    changes made to e65bf29dc4f211b91824649c4995fbdc02efe8d5.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import AppMain from './AppMain';
 import App from './App';
+//import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-
-import './AppMain.css';
 import './dist/output.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppMain />
-      <App />
+  <BrowserRouter>
+    <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
-import reportWebVitals from './reportWebVitals';
